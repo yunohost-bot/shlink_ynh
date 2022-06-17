a1244b6ef66bff782b728f46e1e41ac32a5f37ae
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-06-17 11:31:04 +0200

    First commit

---
+++ scripts/_common.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+YNH_PHP_VERSION="8.0"
+
+pkg_dependencies="php${YNH_PHP_VERSION}-apcu php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-json"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
