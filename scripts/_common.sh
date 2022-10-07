eaed01ef4894d7a6f900ec1af064e6c8209f0d1d
ericgaspar <junk.eg@free.fr>
2022-10-07 20:53:08 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 YNH_PHP_VERSION="8.1"
 
-pkg_dependencies="php${YNH_PHP_VERSION}-apcu php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-json"
+pkg_dependencies="postgresql php${YNH_PHP_VERSION}-apcu php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-json"
 
 #=================================================
 # PERSONAL HELPERS
