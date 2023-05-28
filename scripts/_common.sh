8e163e193f7b654505d5549edceb8aba58dfd67c
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-05-28 20:00:38 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,12 +4,12 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="8.1"
+#REMOVEME? YNH_PHP_VERSION="8.1"
 
-php_dependencies="php${YNH_PHP_VERSION}-apcu php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-json"
+#REMOVEME? php_dependencies="php${YNH_PHP_VERSION}-apcu php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-json"
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="postgresql $php_dependencies"
+#REMOVEME? pkg_dependencies="postgresql $php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
